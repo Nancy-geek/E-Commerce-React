d63fb9a872ee6552cd1a1027664a14975ac80175
Nancy Gupta <nancynishu2305@gmail.com>
2024-09-12 00:05:22 +0530

    uncommented the api call

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 // import { FaUserAlt } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
 
 const Navbar = () => {
     const state = useSelector(state => state.handleCart)
@@ -9,7 +10,7 @@ const Navbar = () => {
         
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"> React Ecommerce</NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"><FaBagShopping /> React Ecommerce</NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -20,7 +21,7 @@ const Navbar = () => {
                             <NavLink className="nav-link" to="/">Home </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/product">Products</NavLink>
+                            <NavLink className="nav-link" to="/products">Products</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/about">About</NavLink>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,23 +19,23 @@ const Products = () => {
     dispatch(addCart(product))
   }
 
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     setLoading(true);
-  //     const response = await fetch("https://fakestoreapi.com/products/");
-  //     if (componentMounted) {
-  //       setData(await response.clone().json());
-  //       setFilter(await response.json());
-  //       setLoading(false);
-  //     }
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products/");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
 
-  //     return () => {
-  //       componentMounted = false;
-  //     };
-  //   };
+      return () => {
+        componentMounted = false;
+      };
+    };
 
-  //   getProducts();
-  // }, []);
+    getProducts();
+  }, []);
 
   const Products = () => {
     const [data, setData] = useState([]);
